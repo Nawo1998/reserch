2983727cd06f5caf6512dded134ed12de28ed689
Nawo1998 <118795956+Nawo1998@users.noreply.github.com>
2023-05-02 15:28:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+  
+
+  return (
+    
+      <div>
+        <Home/>
+        <h1>rghhh</h1>
+      </div>
+      
+    
+  )
+}
+
+export default App
