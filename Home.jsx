7c16b4ecbb6cc9dd6d33c049ae07660159d23bb4
Nawo1998 <118795956+Nawo1998@users.noreply.github.com>
2023-05-02 15:28:57 +0530

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Home.css'
+
+
+export default function Home() {
+  return (
+    <div class="ho1">
+        <div class="ho2">
+            <div class="ho3">
+            <h3>
+                What is telemedicine?
+            </h3>
+            Telehealth refers to providing broader scope of remote healthcare services and technologies than in telemedicine.
+            Telemedicine can be considered as a subset of telehealth and that refers solely to the provision of healthcare services
+            and education over a distance.As result of the advancement of internet and technology, telemedicine is widely used
+            for provoding quality healthcare services when people are unable to access healthcare services physically. 
+            </div>
+            <div class="ho4">
+            <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fechalliance.com%2Fwp-content%2Fuploads%2F2020%2F05%2FSri-Lanka-May-Report-Logos.jpg&tbnid=j1Tmfuw5uOg8-M&vet=12ahUKEwib5tzsntb-AhUqCLcAHZb9CJoQMygGegUIARCIAQ..i&imgrefurl=https%3A%2F%2Fechalliance.com%2Fthe-covid-19-activities-of-the-sri-lanka-connected-health-alliance-slcha-members%2F&docid=yht4Gc6ZIg9flM&w=476&h=152&q=telemedicine%20in%20sri%20lanka&ved=2ahUKEwib5tzsntb-AhUqCLcAHZb9CJoQMygGegUIARCIAQ"/>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
